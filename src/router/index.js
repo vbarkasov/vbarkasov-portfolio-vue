86cbd72a63e9e2179413991320278fded165b5d3
vbarkasov <vbarkasov4dev@gmail.com>
2018-08-13 21:43:47 +0700

    Mode 'history'

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Extensions from '@/pages/Extensions'
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -40,6 +41,10 @@ export default new Router({
       path: '/page/browser-extensions-development',
       name: 'Extensions',
       component: Extensions
-    }
+    }/*,
+    {
+      path: '*',
+      component: NotFoundComponent
+    }*/
   ]
 })
