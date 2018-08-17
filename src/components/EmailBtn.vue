bdcc92b88864da741ffc5381d648300ee7aa49ec
vbarkasov <vbarkasov4dev@gmail.com>
2018-08-17 17:59:53 +0700

    Send email dialog

--- src/components/EmailBtn.vue
+++ src/components/EmailBtn.vue
@@ -14,21 +14,24 @@
             <form class="sendmail-form" name="sendmail-form">
               <div class="form-group">
                 <label for="user-message">Your message *</label>
-                <textarea class="form-control" id="user-message" rows="3" required></textarea>
+                <textarea v-model="message" class="form-control" id="user-message" rows="3" required></textarea>
               </div>
               <div class="form-group">
                 <label for="user-email">Your email for answer *</label>
-                <input type="email" class="form-control" id="user-email" placeholder="name@example.com" required>
+                <input v-model="email" type="email" class="form-control" id="user-email" placeholder="name@example.com" required>
               </div>
               <div class="form-group">
-                <label for="user-email">Your name</label>
-                <input type="email" class="form-control" id="user-name" placeholder="name@example.com">
+                <label for="user-name">Your name</label>
+                <input v-model="name"  type="text" class="form-control" id="user-name" placeholder="Name">
               </div>
               <div class="g-recaptcha" data-sitekey="6LcUkV0UAAAAADuwEqgKLHC5P3lQ1KKx5OiMGXRP"></div>
             </form>
+            <div v-if="userMessage" :class="['alert-' + userMessageType]" class="mb-0 mt-3 alert">
+              {{ userMessage }}
+            </div>
           </div>
           <div class="modal-footer">
-            <button v-on:click.prevent="submitForm" type="submit" class="btn btn-primary">Send</button>
+            <button v-on:click.prevent="sendMail" type="submit" class="btn btn-primary">Send</button>
             <button v-on:click.prevent="popupIsVisible = false" type="button" class="btn btn-secondary">Cancel</button>
           </div>
         </div>
@@ -43,32 +46,40 @@ export default {
   data () {
     return {
       popupIsVisible: false,
-      successMessage: ''
+      userMessage: '',
+      userMessageType: '',
+      message: '',
+      email: '',
+      name: ''
     }
   },
   methods: {
     showPopup: function(e){
       e.preventDefault();
+      this.userMessage = '';
+      this.userMessageType = '';
       this.popupIsVisible = true;
     },
-    submitForm: function(){
-
-    },
     sendMail: function() {
-        $.ajax({
-          type:'POST',
-          url:'/sendmail.php',
-          dataType: 'json',
-          data: {
-            name: $('#user-name').val(),
-            email: $('#user-email').val(),
-            message: $('#user-message').val(),
-            'g-recaptcha-response': $('#g-recaptcha-response').val()
-          },
-          success: function(data){
-            $(".email-success-message").append(data.success).fadeIn(999);
-          }
-        });
+      const self = this;
+
+      $.ajax({
+        type:'POST',
+        url:'/sendmail.php',
+        dataType: 'json',
+        data: {
+          name: self.name,
+          email: self.email,
+          message: self.message,
+          'g-recaptcha-response': document.getElementById('g-recaptcha-response').value
+        }
+      }).done(function(data, textStatus, jqXHR) {
+        self.userMessage = 'The message is sent successfully';
+        self.userMessageType = 'success';
+      }).fail(function(jqXHR, textStatus, errorThrown ) {
+        self.userMessage = 'The message is not delivered';
+        self.userMessageType = 'danger';
+      });
     }
   },
   mounted() {
@@ -80,7 +91,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .modal.showed {
     display: block;
