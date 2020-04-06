4e2d56e1de8728571c9193c52c9df3357d3806b1
Vladimir Barkasov <vbarkasov4dev@gmail.com>
2020-04-06 20:42:32 +0700

    Fix sending a message from the site with recaptcha

--- server/sendmail.php
+++ server/sendmail.php
@@ -1,8 +1,12 @@
 <?php
-$mail_address = '';
-$mail_subject = 'Email from site ' . $_SERVER['SERVER_NAME'];
+$mail_address = ''; // type your target email
+$mail_subject = 'Сообщение с сайта ' . $_SERVER['SERVER_NAME'];
 $mail_text = '';
-$recaptcha_secret = '';
+$recaptcha_secret = ''; // type your recaptcha secret
+
+$rest_json = file_get_contents('php://input');
+$_POST = json_decode($rest_json, true);
+
 
 function add_to_mail_text($title, $value){
     global $mail_text;
@@ -10,9 +14,9 @@ function add_to_mail_text($title, $value){
 }
 
 $inputs = array(
-    array('name', 'Name'),
+    array('name', 'Имя'),
     array('email', 'Email'),
-    array('message', 'Message'),
+    array('message', 'Сообщение'),
 );
 
 foreach ($inputs as $input){
@@ -21,70 +25,29 @@ foreach ($inputs as $input){
     }
 }
 
-$attachment = '';
-
-if (!empty($_FILES['file']['tmp_name'])) {
-    $path = $_FILES['file']['name'];
-    if (copy($_FILES['file']['tmp_name'], $path)) $attachment = $path;
-}
-
-function send(){
-    global $mail_address, $mail_subject, $mail_text, $redirect, $mail, $attachment;
-    if(empty($attachment)){
-        $headers = "MIME-Version: 1.0\r\n";
-        $headers .= "Content-type: text/html; charset=utf-8\r\n";
-        $headers .= 'From: no-reply@'.$_SERVER['SERVER_NAME']."\r\n";
-        $multipart = $mail_text;
-    } else {
-        $boundary = "--".md5(uniqid(time()));
-        $headers = "MIME-Version: 1.0\r\n";
-        $headers .= "Content-type: multipart/mixed; boundary=\"$boundary\"\r\n";
-        $headers .= 'From: no-reply@'.$_SERVER['SERVER_NAME']."\r\n";
-        $multipart = "--$boundary\r\n";
-        $multipart .= "Content-type: text/html; charset=utf-8\r\n";
-        $multipart .= "Content-Transfer-Encoding: base64\r\n";
-        $multipart .= "\r\n";
-        $multipart .= chunk_split(base64_encode($mail_text));
-        $fp = fopen($attachment,"r");
-        $file = fread($fp, filesize($attachment));
-        fclose($fp);
-        $message_part = "\r\n--$boundary\r\n";
-        $message_part .= "Content-Type: application/octet-stream; name=\"$attachment\"\r\n";
-        $message_part .= "Content-Transfer-Encoding: base64\r\n";
-        $message_part .= "Content-Disposition: attachment; filename=\"$attachment\"\r\n";
-        $message_part .= "\r\n";
-        $message_part .= chunk_split(base64_encode($file));
-        $message_part .= "\r\n--$boundary--\r\n";
-        $multipart .= $message_part;
-    }
+function send() {
+    global $mail_address, $mail_subject, $mail_text;
+    $headers = 'MIME-Version: 1.0\r\n';
+    $headers .= 'Content-type: text/html; charset=utf-8\r\n';
+    $headers .= 'From: no-reply@'.$_SERVER['SERVER_NAME'].'\r\n';
+    $multipart = $mail_text;
     $mail = mail($mail_address, $mail_subject, $multipart, $headers);
-    if(!empty($attachment)){
-        if (time_nanosleep(0, 500000000)) {
-            unlink($attachment);
-        }
-    }
 
-    $is_ajax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
-    if($is_ajax){
-        header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
-        if($mail){
-            echo json_encode(array("status" => "success"));
-        }else{
-            echo json_encode(array("status" => "error"));
-        }
+    header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+    if($mail) {
+      echo json_encode(array('status' => 'success'));
     } else {
-        $redirect_url = isset($_POST['redirect']) ? $_POST['redirect'] : $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER['SERVER_NAME'];
-        header('Location: '.$redirect_url);
+      echo json_encode(array('status' => 'error'));
     }
 }
 
-//recaptcha
 if(isset($_POST['g-recaptcha-response'])) {
     $url = 'https://www.google.com/recaptcha/api/siteverify';
     $params = array(
         'secret' => $recaptcha_secret,
         'response' => $_POST['g-recaptcha-response'],
     );
+
     $result_str = file_get_contents($url, false, stream_context_create(array(
         'http' => array(
             'method'  => 'POST',
@@ -93,12 +56,14 @@ if(isset($_POST['g-recaptcha-response'])) {
         )
     )));
 
-    if($result_str){
+    if($result_str) {
         $result = json_decode($result_str);
         if($result->success){
             send();
         }
     } else {
-        echo json_encode(array("status" => "error"));
+        echo json_encode(array('status' => 'error'));
     }
+} else {
+    echo json_encode(array('status' => 'error'));
 }
--- src/components/SendMessagePopup.vue
+++ src/components/SendMessagePopup.vue
@@ -9,30 +9,47 @@
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">
-            <form class="sendmail-form" name="sendmail-form" method="POST">
-              <div class="form-group">
-                <label for="user-message">Your message *</label>
-                <textarea v-model="message" class="form-control" id="user-message" rows="3" required></textarea>
+          <form
+            class="sendmail-form"
+            name="sendmail-form"
+            @submit.prevent="onSubmit"
+            method="POST"
+          >
+            <div class="modal-body">
+                <div class="form-group">
+                  <label for="user-message">Your message *</label>
+                  <textarea v-model="message" class="form-control" id="user-message" rows="3" required></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="user-email">Your email for answer *</label>
+                  <input v-model="email" type="email" class="form-control" id="user-email" placeholder="name@example.com" required>
+                </div>
+                <div class="form-group">
+                  <label for="user-name">Your name</label>
+                  <input v-model="name"  type="text" class="form-control" id="user-name" placeholder="Name">
+                </div>
+                <vue-recaptcha
+                  ref="recaptcha"
+                  :sitekey="gRecaptchaSitekey"
+                  @expired="onCaptchaExpired"
+                  @verify="sendMail"
+                ></vue-recaptcha>
+              <div v-if="userMessage" :class="['alert-' + userMessageType]" class="mb-0 mt-3 alert">
+                {{ userMessage }}
               </div>
-              <div class="form-group">
-                <label for="user-email">Your email for answer *</label>
-                <input v-model="email" type="email" class="form-control" id="user-email" placeholder="name@example.com" required>
-              </div>
-              <div class="form-group">
-                <label for="user-name">Your name</label>
-                <input v-model="name"  type="text" class="form-control" id="user-name" placeholder="Name">
-              </div>
-              <vue-recaptcha :sitekey="gRecaptchaSitekey"></vue-recaptcha>
-            </form>
-            <div v-if="userMessage" :class="['alert-' + userMessageType]" class="mb-0 mt-3 alert">
-              {{ userMessage }}
             </div>
-          </div>
-          <div class="modal-footer">
-            <button v-on:click.prevent="sendMail" type="submit" class="btn btn-primary">Send</button>
-            <button v-on:click.prevent="emailPopupIsShown = false" type="button" class="btn btn-secondary">Cancel</button>
-          </div>
+            <div class="modal-footer">
+              <button type="submit" class="btn btn-primary">
+                <span v-if="emailSent">
+                  Send again
+                </span>
+                <span v-else>
+                  Send
+                </span>
+              </button>
+              <button v-on:click.prevent="emailPopupIsShown = false" type="button" class="btn btn-secondary">Cancel</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -41,7 +58,8 @@
 
 <script>
 import VueRecaptcha from 'vue-recaptcha'
-import gRecaptchaSitekey from '../config'
+import { gRecaptchaSitekey } from '../config'
+console.log(gRecaptchaSitekey)
 
 export default {
   'name': 'SendMessagePopup',
@@ -55,29 +73,40 @@ export default {
       'message': '',
       'email': '',
       'name': '',
-      'gRecaptchaSitekey': gRecaptchaSitekey
+      'gRecaptchaSitekey': gRecaptchaSitekey,
+      'emailSent': false
     }
   },
   'methods': {
-    'showPopup': function (e) {
+    showPopup (e) {
       e.preventDefault()
       this.userMessage = ''
       this.userMessageType = ''
       this.$store.dispatch('setEmailPopupVisibilityStatus', true)
     },
-    'hidePopup': function () {
+    hidePopup () {
       this.userMessage = ''
       this.userMessageType = ''
       this.$store.dispatch('setEmailPopupVisibilityStatus', false)
     },
-    'sendMail': function () {
+    onSubmit () {
+      document.querySelector('[name="g-recaptcha-response"]').click()
+      const el = document.querySelector('[name="g-recaptcha-response"]')
+      if (el.value === '') {
+        this.userMessage = 'Please confirm that you are human'
+        this.userMessageType = 'danger'
+      } else {
+        this.sendMail(el.value)
+      }
+    },
+    sendMail (recaptchaToken) {
       const self = this
 
-      const recaptchaResponse = document.getElementById('g-recaptcha-response')
-        ? document.getElementById('g-recaptcha-response').value
-        : ''
-      // console.log('this.$refs.recaptcha');
-      // console.log(this.$refs.recaptcha);
+      if (self.message === '' || self.email === '' || self.name === '') {
+        self.userMessage = 'Please enter all data before sending a message'
+        self.userMessageType = 'danger'
+        return
+      }
 
       fetch('/sendmail.php', {
         'method': 'POST',
@@ -89,18 +118,22 @@ export default {
           'name': self.name,
           'email': self.email,
           'message': self.message,
-          'g-recaptcha-response': recaptchaResponse
+          'g-recaptcha-response': recaptchaToken
         })
       })
         .then(response => response.json())
         .then(() => {
           self.userMessage = 'The message is sent successfully'
           self.userMessageType = 'success'
+          self.emailSent = true
         })
         .catch(() => {
           self.userMessage = 'The message is not delivered'
           self.userMessageType = 'danger'
         })
+    },
+    onCaptchaExpired () {
+      this.$refs.recaptcha.reset()
     }
   },
   'computed': {
@@ -114,18 +147,21 @@ export default {
     }
   },
   mounted () {
-    const self = this
     const recaptchaScript = document.createElement('script')
-    recaptchaScript.src = 'https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit'
+    // recaptchaScript.src = 'https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit'
+    recaptchaScript.src = 'https://www.google.com/recaptcha/api.js?render=explicit'
     recaptchaScript.async = true
     recaptchaScript.defer = true
     document.head.appendChild(recaptchaScript)
 
-    window.onloadCallback = function () {
-      window.grecaptcha && window.grecaptcha.render('captcha', {
-        'sitekey': self.gRecaptchaSitekey
+    /* window.onloadCallback = function () {
+      return new
+      Promise(function (resolve, reject) {
+        resolve()
       })
-    }
+    } */
+
+    this.emailSent = false
   }
 }
 </script>
